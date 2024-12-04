266093e7c52ba5b76fd4322d56751bedeeccfdb6
Patrik Mintěl <ja@patrick115.eu>
2024-12-04 16:26:12 +0100

    Day 4 done

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,10 +1,7 @@
 use aoc_2024::load_file;
 
-fn main() -> anyhow::Result<()> {
-    let load_file = load_file(4);
-    let lines: Vec<&str> = load_file.lines().collect();
-
-    let increment: Vec<(i8, i8)> = vec![
+fn star1(lines: &Vec<&str>) -> u32 {
+    let increment: Vec<(i32, i32)> = vec![
         (1,0), // forward
         (-1, 0), //backward
         (0,1), //down
@@ -16,14 +13,17 @@ fn main() -> anyhow::Result<()> {
     ];
 
     let word = "XMAS";
+    let word_size = word.len() as i32;
 
     let mut words = 0;
 
     for y in 0..lines.len() {
-        for x in 0..lines[y].len() {
+        let width = lines[y].len() as i32;
+        for x in 0..width {
             for inc in &increment {
                 //check if possible
-                let (last_x, last_y) = (x as i32 + word.len() as i32 * inc.0 as i32, y as i32 + word.len() as i32 * inc.0 as i32);
+                let last_x = x + ((word_size - 1) * inc.0);
+                let last_y = y as i32 + ((word_size - 1) * inc.1);
                 if last_x < 0 || last_x >= lines[y].len() as i32 || last_y < 0 || last_y >= lines.len() as i32 {
                     continue; 
                 }
@@ -34,24 +34,115 @@ fn main() -> anyhow::Result<()> {
                 //try search
                 let mut found = true;
                 for c in word.chars() {
-                    if lines[curr_y as usize].chars()[curr_x as usize] != c {
-                        found = false;
-                        break;
-                    }
+                    if let Some(char) = lines[curr_y as usize].chars().nth(curr_x as usize) {
+                        if char != c {
+                            found = false;
+                            break;
+                        }
 
-                    curr_x += inc.0 as i32;
-                    curr_y += inc.1 as i32;
+                        curr_x += inc.0 as i32;
+                        curr_y += inc.1 as i32;
+                        continue;
+                    }
+                    break;
                 }
 
                 if !found {
                     continue;
-                }   
+                }  
+
                 words += 1;
             }    
         }
     }
-    //println!("1. star {safe}");
-    //println!("2. star {safe_module}");
+
+    words
+}
+
+fn star2(lines: &Vec<&str>) -> u32 {
+    let offsets: Vec<Vec<(i32, i32, char)>> = vec![
+        vec![
+            (-1, -1, 'M'),
+            (1, 1, 'S'),
+            (1, -1, 'M'),
+            (-1, 1, 'S')
+        ],
+        vec![
+            (-1, -1, 'M'),
+            (1, 1, 'S'),
+            (1, -1, 'S'),
+            (-1, 1, 'M')
+        ],
+        vec![
+            (-1, -1, 'S'),
+            (1, 1, 'M'),
+            (1, -1, 'M'),
+            (-1, 1, 'S')
+        ],
+        vec![
+            (-1, -1, 'S'),
+            (1, 1, 'M'),
+            (1, -1, 'S'),
+            (-1, 1, 'M')
+        ]
+    ];
+
+    let height = lines.len() as i32;
+    
+    let mut x_mases = 0;
+
+    for y in 0..lines.len() {
+        let width = lines[y].len() as i32;
+
+        for x in 0..lines[y].len() {
+            if let Some(char) = lines[y].chars().nth(x) {
+                if char != 'A' {
+                    continue;
+                }
+
+                let x = x as i32;
+                let y = y as i32;
+
+                //check
+                if x - 1 < 0 || x + 1 >= width || y - 1 < 0 || y + 1 >= height  {
+                    continue;
+                }
+
+                for offset_list in &offsets {
+                    let mut valid = true;
+                    for offset in offset_list {
+                        if let Some(c) = lines[(y + offset.1) as usize].chars().nth((x + offset.0) as usize) {
+                            if c != offset.2 {
+                                valid = false;
+                                break;
+                            }
+                            continue;
+                        }
+                        valid = false;
+                        break;
+                    }
+
+                    if !valid {
+                        continue;
+                    }
+
+                    x_mases += 1;
+                }
+                
+            } 
+        }
+    }
+
+    x_mases
+}
+
+fn main() -> anyhow::Result<()> {
+    let load_file = load_file(4);
+    let lines: Vec<&str> = load_file.lines().collect();
+
+    
+    println!("1. star {}", star1(&lines));
+    println!("2. star {}", star2(&lines));
 
     Ok(())
 }
